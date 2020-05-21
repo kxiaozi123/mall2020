54e69343a5c659608e552b3caadaab5615eda328
K <511158469@qq.com>
2020-05-21 22:23:26 +0800

    初始化

---
+++ src/main/java/com/imooc/mall2020/Mall2020Application.java
@@ -0,0 +1,13 @@
+package com.imooc.mall2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mall2020Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mall2020Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/imooc/mall2020/Mall2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.mall2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mall2020ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
